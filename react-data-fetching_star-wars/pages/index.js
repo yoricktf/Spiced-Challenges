e093cb2d192400c3766173d3a21d72e2212d4e0f
yoricktf <yorick.tenfeld@gmail.com>
2023-02-21 09:56:08 +0100

    star wars swr challenge

--- react-data-fetching_star-wars/pages/index.js
+++ react-data-fetching_star-wars/pages/index.js
@@ -3,8 +3,6 @@ import Link from "next/link";
 import Layout from "../components/Layout";
 
 export default function HomePage() {
-
-
   return (
     <Layout>
       <h1>React Data Fetching: Star Wars</h1>
